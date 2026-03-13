5958185cc9e602b9a8ed248d075a2a889cca8da8
jajatlesmana3-hash <jajatlesmana3@gmail.com>
2026-03-14 04:24:57 +0700

    Update test_detect_data.R

--- tests/testthat/test_detect_data.R
+++ tests/testthat/test_detect_data.R
@@ -1,1 +1,24 @@
-
+test_that("detect_data_structure works with various inputs", {
+  
+  # data.frame
+  df <- data.frame(
+    num = rnorm(50),
+    fac = factor(sample(letters[1:3], 50, replace = TRUE)),
+    char = sample(c("x","y"), 50, replace = TRUE),
+    int = 1:50
+  )
+  
+  info <- detect_data_structure(df, "num", "fac")
+  expect_equal(info$dependent$name, "num")
+  expect_equal(info$dependent$type_info$main_type, "NUMERIC")
+  expect_equal(info$independent$type_info$main_type, "CATEGORICAL")
+  
+  # vector
+  vec <- rnorm(30)
+  info <- detect_data_structure(vec, "value")
+  expect_equal(info$dependent$name, "value")
+  expect_equal(info$dependent$type_info$main_type, "NUMERIC")
+  
+  # missing column error
+  expect_error(detect_data_structure(df, "nonexistent"), "not found")
+})
