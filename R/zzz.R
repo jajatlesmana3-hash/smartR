111d83d4ea2725c1aad0bf33f6dd296af6a5c33c
jajatlesmana3-hash <jajatlesmana3@gmail.com>
2026-03-14 04:21:32 +0700

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,25 @@
+#' .onAttatch hook for smartR
+#'
+#' This function runs when the package is attached via library(smartR).
+#' It displays a welcome message with copyright and quick start information.
+#'
+#' @param libname Library name
+#' @param pkgname Package name
+#' @keywords internal
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("
+smartR version 0.1.0
+Copyright (c) 2026 Jajat L
+License: MIT
 
+smartR helps you choose the right statistical test based on your data.
+
+Quick start:
+  result <- smart_recommend(iris, 'Sepal.Length', 'Species')
+  result
+
+For detailed documentation:
+  vignette('introduction', package = 'smartR')
+  https://github.com/jajatlesmana3-hash/smartR
+")
+}
